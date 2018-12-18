4e8877fb40c14992b9a5e9a8c34f15d321875730
hamdi <javs_shao@163.com>
2018-12-18 16:13:35 +0800

    指定状态码信息

--- scrapydownloadertest/scrapydownloadertest/middlewares.py
+++ scrapydownloadertest/scrapydownloadertest/middlewares.py
@@ -115,3 +115,7 @@ class RandomUserAgentMiddleware():
 
     def process_request(self, request, spider):
         request.headers['User-Agent'] = random.choice(self.user_agents)
+
+    def process_response(self, request, response, spider):
+        response.status = 201
+        return response
--- scrapydownloadertest/scrapydownloadertest/spiders/httpbin.py
+++ scrapydownloadertest/scrapydownloadertest/spiders/httpbin.py
@@ -9,3 +9,4 @@ class HttpbinSpider(scrapy.Spider):
 
     def parse(self, response):
         self.logger.debug(response.text)
+        self.logger.debug('状态码:' + str(response.status))
