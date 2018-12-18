6bf6f4ba6a4599ce968510471f1a72a66bfcd952
hamdi <javs_shao@163.com>
2018-12-18 16:06:08 +0800

    指定随机User-Agent

--- scrapydownloadertest/scrapydownloadertest/settings.py
+++ scrapydownloadertest/scrapydownloadertest/settings.py
@@ -90,4 +90,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # 指定User-Agent
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+# USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
